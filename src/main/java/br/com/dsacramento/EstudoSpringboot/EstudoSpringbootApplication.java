f5ecdae73b97256bf7451625337625533f1e4245
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-01-18 20:30:48 -0300

    first commit

---
+++ src/main/java/br/com/dsacramento/EstudoSpringboot/EstudoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.dsacramento.EstudoSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringbootApplication.class, args);
+	}
+
+}

d444a9c004cdd8e3bd497e3c255bb403ed096f2c
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-01-25 14:50:39 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel (Aula 3)

---
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,15 @@
+package br.com.dsacramento.CadastroDeNInjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+
+}
+
+// Última aula assistida #P66 , Começar na seguinte!
--- src/test/java/br/com/dsacramento/CadastroDeNInjas/EstudoSpringbootApplicationTests.java
+++ src/test/java/br/com/dsacramento/CadastroDeNInjas/EstudoSpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.dsacramento.EstudoSpringboot;
+package br.com.dsacramento.CadastroDeNInjas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

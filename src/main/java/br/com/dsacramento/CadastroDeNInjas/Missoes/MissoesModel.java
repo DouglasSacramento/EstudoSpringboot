dc053e729f8bf5993699a53232c7948d77e2c358
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-01-25 16:47:32 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/br/com/dsacramento/CadastroDeNInjas/Missoes/MissoesModel.java
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package br.com.dsacramento.CadastroDeNInjas.Missoes;
 
 import br.com.dsacramento.CadastroDeNInjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -20,5 +26,4 @@ public class MissoesModel {
     // @OneToMany Uma missão pode ter vários ninjas.
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
 }
--- src/main/java/br/com/dsacramento/CadastroDeNInjas/Ninjas/NinjaModel.java
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/Ninjas/NinjaModel.java
@@ -2,9 +2,13 @@ package br.com.dsacramento.CadastroDeNInjas.Ninjas;
 
 import br.com.dsacramento.CadastroDeNInjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cadastro_ninjas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -22,40 +26,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreing key ou chave estrangeira.
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }

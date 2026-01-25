bf7d8deb669a62d540056b723a6d24a9ddb2510a
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-01-25 18:59:14 -0300

    feat: Implementa as PACKAGES realacionadas a arquitetura de camadas, Também inclui o JpaRepository nas INTERFACES dos repository de  missões e ninjas

---
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/Missoes/MissoesController.java
@@ -0,0 +1,4 @@
+package br.com.dsacramento.CadastroDeNInjas.Missoes;
+
+public class MissoesController {
+}
---
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/Ninjas/NinjaService.java
@@ -0,0 +1,4 @@
+package br.com.dsacramento.CadastroDeNInjas.Ninjas;
+
+public class NinjaService {
+}

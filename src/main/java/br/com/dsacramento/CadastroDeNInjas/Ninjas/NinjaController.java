8154b196e501913e8f81906957b615eb46b637d8
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-01-31 16:10:49 -0300

    feat: Implementa as rotas do tipo REST no controller

--- src/main/java/br/com/dsacramento/CadastroDeNInjas/Ninjas/NinjaController.java
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/Ninjas/NinjaController.java
@@ -1,16 +1,36 @@
 package br.com.dsacramento.CadastroDeNInjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
-
-    @GetMapping("/boasvindas")
-    public String boasVindas(){
-        return "Essa é minha primeira menssagem nessa rota!";
+    // Adicionar
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado";
     }
 
+    // Mostrar todos
+    @GetMapping("/todos")
+    public String mostrarNinjas(){
+        return "Mostrar ninjas";
+    }
+
+    // Mostrar por Id
+    @GetMapping("/todos/{id}")
+    public String mostrarNinjaPorId(){
+        return "Mostrar ninja por id";
+    }
+
+    // Alterar
+    @PutMapping("/alterar/{id}")
+    public String alterarNinjaPorId(){
+        return "Alterar ninja por id";
+    }
+    // Deletar
+    @DeleteMapping("/deletar/{id}")
+    public String deletarNinjaPorId(){
+        return "Deletar ninja por id";
+    }
 }

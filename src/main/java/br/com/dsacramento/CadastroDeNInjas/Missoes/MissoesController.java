0846ff9903203bcde2e624149f3bec6af40e5705
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-02-01 14:15:04 -0300

    feat: Cria as rotas de listar todas missões e listar missões por Id.

--- src/main/java/br/com/dsacramento/CadastroDeNInjas/Missoes/MissoesController.java
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/Missoes/MissoesController.java
@@ -1,4 +1,30 @@
 package br.com.dsacramento.CadastroDeNInjas.Missoes;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
+    // Listar missoes
+    @GetMapping("/listar")
+    public List<MissoesModel> listarMissoes() {
+        return missoesService.listarMissoes();
+    }
+
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissaoPorId(@PathVariable Long id) {
+        return missoesService.listarMissaoPorId(id);
+    }
 }
--- src/main/java/br/com/dsacramento/CadastroDeNInjas/Missoes/MissoesService.java
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/Missoes/MissoesService.java
@@ -1,4 +1,28 @@
 package br.com.dsacramento.CadastroDeNInjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Listar missoes
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+    // Listar missoes por Id
+    public MissoesModel listarMissaoPorId(Long id){
+        Optional<MissoesModel> missao = missoesRepository.findById(id);
+        return missao.orElseThrow(() -> new RuntimeException("Missão not found"));
+    }
+
 }

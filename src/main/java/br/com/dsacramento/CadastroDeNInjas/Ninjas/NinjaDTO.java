c327cc86a01a1563d4cfa4dfa72029fb1e94c994
Douglas Sacramento <douglasj.sacramento@gmail.com>
2026-02-01 23:29:12 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidades x dto.

--- src/main/java/br/com/dsacramento/CadastroDeNInjas/Ninjas/NinjaDTO.java
+++ src/main/java/br/com/dsacramento/CadastroDeNInjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package br.com.dsacramento.CadastroDeNInjas.Ninjas;
+import br.com.dsacramento.CadastroDeNInjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+    private UUID id;
+    private String nome;
+    private int idade;
+    private String rank;
+    private String email;
+    private String imgUrl;
+    private MissoesModel missoes;
 }
